95b17508519f86e0130d1e8116bd6eca54348a8f
Rahulstar177 <rathodsachiit@gmaiil.com>
2022-12-09 21:36:16 +0530

    old task

--- Test/src/module-info.java
+++ Test/src/module-info.java
@@ -7,8 +7,44 @@
  */
 module Test {
 	
+<<<<<<< Updated upstream
 //	public void Test()
 //	{
 //		
 //	}
+=======
+	
+	
+	
+	/*10 to 12
+	 * 
+	 * test
+	 * code
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	/*12  to 2 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	
+>>>>>>> Stashed changes
 }
